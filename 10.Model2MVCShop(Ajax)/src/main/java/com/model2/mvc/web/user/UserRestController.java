73cd314efaa6316f818f0193d886c879c869b0fe
bitcamp <bitcamp@DESKTOP-RJJ5KHS>
2023-03-16 21:46:56 +0900

    03/16

---
+++ 10.Model2MVCShop(Ajax)/src/main/java/com/model2/mvc/web/user/UserRestController.java
@@ -0,0 +1,87 @@
+package com.model2.mvc.web.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.User;
+import com.model2.mvc.service.user.UserService;
+
+
+//==> 회원관리 RestController 10
+@RestController
+@RequestMapping("/user/*")
+public class UserRestController {
+	
+	///Field
+	@Autowired
+	@Qualifier("userServiceImpl")
+	private UserService userService;
+	//setter Method 구현 않음
+		
+	public UserRestController(){
+		System.out.println(this.getClass());
+	}
+	
+	@RequestMapping( value="json/getUser/{userId}", method=RequestMethod.GET )
+	public User getUser( @PathVariable String userId ) throws Exception{
+		
+		System.out.println("/user/json/getUser : GET");
+		
+		//Business Logic
+		return userService.getUser(userId);
+	}
+
+	@RequestMapping( value="json/login", method=RequestMethod.POST )
+	public User login(	@RequestBody User user,
+									HttpSession session ) throws Exception{
+	
+		System.out.println("/user/json/login : POST");
+		//Business Logic
+		System.out.println("::"+user);
+		User dbUser=userService.getUser(user.getUserId());
+		
+		// 아이디가 없다면.
+		if( dbUser==null ) {
+			dbUser = new User();
+		}
+		
+		if( user.getPassword().equals(dbUser.getPassword())){
+			session.setAttribute("user", dbUser);
+		}
+	
+		return dbUser;
+	}
+	//RequestParam("searchCondition") String searchCondition ,@RequestParam("searchKeyword") String searchKeyword 
+	@RequestMapping( value="json/getList", method=RequestMethod.POST  )
+	public List<String> getList( @RequestBody Search search ) throws Exception{
+		System.out.println("#############################################################################################################");
+		System.out.println(search);
+		List<User> li = userService.getList(search.getSearchCondition(),search.getSearchKeyword()); 
+		System.out.println(li);
+		List<String> li2 = new ArrayList<String>();
+		for(User l : li) {
+			li2.add(l.getUserId());
+		}
+		System.out.println(li2);
+		return li2;
+	}
+
+}
